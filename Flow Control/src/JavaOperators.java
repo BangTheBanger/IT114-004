8b5daac34c64060d78d32938750ec207368e71d6
Filipe A <nullnight123@gmail.com>
2021-02-09 17:35:12 -0500

    HW-FlowControl

---
+++ Flow Control/src/JavaOperators.java
@@ -0,0 +1,16 @@
+public class JavaOperators{
+	 public static void main(String[] args){
+		 System.out.println("Java Exercise [Operators] Exercise [1]");
+		 System.out.println(10*5 + "\n");
+		 System.out.println("Java Exercise [Operators] Exercise [2]");
+		 System.out.println(10/5 + "\n");
+		 System.out.println("Java Exercise [Operators] Exercise [3]");
+		 int x = 10;
+		 ++x;
+		 System.out.println(x + "\n");
+		 System.out.println("Java Exercise [Operators] Exercise [4]");
+		 x = 10;
+		 x += 5;
+		 System.out.println(x + "\n");
+	 }
+}
